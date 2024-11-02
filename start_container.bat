97d65fcb9b567f7a8efc56c5cd1dabc926423d2c
ashokvyas <ashokvyas82@gmail.com>
2024-11-02 12:12:23 +0530

    Update and rename start_container.sh to start_container.bat

--- start_container.bat
+++ start_container.bat
@@ -1,8 +1,7 @@
-#!/bin/bash
-set -e
+echo on
 
-# Pull the Docker image from Docker Hub
 docker pull ashokvyas/aws-sample-node-app
 
-# Run the Docker image as a container
 docker run -d -p 3000:3000 ashokvyas/aws-sample-node-app
+
+pause
